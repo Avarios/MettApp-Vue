4a4373ee1ec660ab2098d8cb2702b0351d8b4d0d
Pawel Warmuth <warmuth.pawel@gmail.com>
2019-01-10 22:10:08 +0100

    Made some Boilerplate Toolbar etc

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,22 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">
-          MATERIAL DESIGN
-        </span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">
-          Latest Release
-        </span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <Toolbar />
 
     <v-content>
-      <HelloWorld />
+      <EventList />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import EventList from './components/EventList'
+import Toolbar from './components/Toolbar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EventList,
+    Toolbar
   },
   data () {
     return {
