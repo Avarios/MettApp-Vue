9e3dce4ebbdb741cd5bf0dd9a45cdfccb35ff83b
Pawel Warmuth <warmuth.pawel@gmail.com>
2019-01-13 17:10:41 +0100

    Added Dialog Options and removed PINK Color !

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-bind:value="show" persistent>
+  <v-dialog v-bind:value="show" persistent transition lazy full-width dark fullscreen hide-overlay> 
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
